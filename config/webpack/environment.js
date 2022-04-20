2175a7b7ba5979b3af5551af03a4d9eec203a311
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-20 10:46:20 +0000

    [Fix] bootstrap反映

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,10 +4,10 @@ module.exports = environment
 
 const webpack = require('webpack')
 environment.plugins.prepend(
-	'Provide',
-	new webpack.ProvidePlugin({
-		$: 'jquery/src/jquery',
-		jQuery: 'jquery/src/jquery',
-		Popper: 'popper.js'
-	})
-	)
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
